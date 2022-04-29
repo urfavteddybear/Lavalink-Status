e6935dfe9c1b72fa485d73f1b42c64651555fe81
urfavteddybear <97431620+urfavteddybear@users.noreply.github.com>
2022-04-29 21:48:25 +0800

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    token: process.env.token || "",
+    channel: process.env.channel || "",
+
+    nodes: [
+        {
+            host: "",
+            password: "",
+            port: 0000,
+            retryDelay: 300000,
+            identifier: "",
+            secure: false/true
+        },
+        {
+            host: "",
+            password: "",
+            port: 0000,
+            retryDelay: 300000,
+            identifier: "",
+            secure: false/true
+        },
+        {
+            host: "",
+            password: "",
+            port: 0000,
+            retryDelay: 300000,
+            identifier: "",
+            secure: false/true
+        },
+        {
+            host: "",
+            password: "",
+            port: 0000,
+            retryDelay: 300000,
+            identifier: "",
+            secure: false/true
+        },
+        {
+            host: "",
+            password: "",
+            port: 0000,
+            retryDelay: 300000,
+            identifier: "",
+            secure: false/true
+        },
+        
+      
+    ]
+} 
---
+++ src/index.js
@@ -0,0 +1,60 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { readdirSync } = require("fs");
+const { Manager } = require("erela.js");
+const { token, nodes, retryDelay, retryAmount } = require("./config");
+const colors = require("colors");
+
+const client = new Client({
+  disableMentions: "everyone",
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: [GatewayIntentBits.Guilds],
+});
+
+process.on("unhandledRejection", (error) => {
+  return;
+  // console.log(error) Either you want to log the error or not
+});
+process.on("uncaughtException", (error) => {
+  return;
+  // console.log(error) Either you want to log the error or not
+});
+
+client.manager = new Manager({
+  nodes,
+  retryDelay,
+  retryAmount,
+  send: (id, payload) => {
+    const guild = client.guilds.cache.get(id);
+    if (guild) guild.shard.send(payload);
+  },
+})
+  .on("nodeConnect", (node) =>
+    console.log(
+      colors.green(
+        `[NODE] ${node.options.identifier} | Lavalink node is connected.`
+      )
+    )
+  )
+  .on("nodeReconnect", (node) =>
+    console.log(
+      colors.green(
+        `[NODE] ${node.options.identifier} | Lavalink node is reconnecting.`
+      )
+    )
+  )
+  .on("nodeDisconnect", (node) =>
+    console.log(
+      colors.green(
+        `[NODE] ${node.options.identifier} | Lavalink node is disconnected.`
+      )
+    )
+  );
+
+readdirSync("./src/events/").forEach((file) => {
+  const event = require(`./events/${file}`);
+  let eventName = file.split(".")[0];
+  console.log(colors.green(`[EVENTS] Loaded [${client._eventsCount}] events`));
+  client.on(eventName, event.bind(null, client));
+});
+
+client.login(token);
